39b8e30a6cee5d24630fd0fc5b66a786c77df9c8
NIKHIL AGRAWAL <nikhilagrawal0605@gmail.com>
2022-12-17 11:01:39 +0530

    add
    add

---
+++ c++ learning/average.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	/* code */
+	int physics=80;
+	int chemistry =90;
+	int maths=40;
+	float avg= (physics+maths+chemistry)/3;
+	cout<<"average"<<avg;
+	
+}
