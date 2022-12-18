41d5e7d6ef3ca58eb55de00e3960996be5291a50
NIKHIL AGRAWAL <nikhilagrawal0605@gmail.com>
2022-12-18 14:58:27 +0530

    add

---
+++ c++/Day 1 18 dec 2022/average.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+   /*/ int physics=90;
+    int chemistry=80;
+    int maths=50;
+    float average= (physics+chemistry+maths)/3.0;
+    cout<<"average= "<<average;*/
+    int p,c,m;
+    cin>>p>>c>>m;
+    float average=(c+p+m)/3.0;
+    cout<<"average="<<average;
+}
