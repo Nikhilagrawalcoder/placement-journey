9868b0d97036d79d8debec7144ed1515283e2b06
Nikhil <nikhilagrawal0605@gmail.com>
2023-02-01 21:49:04 +0530

    commit

---
+++ python learning/apnacollegetutorial/basic.py
@@ -0,0 +1,9 @@
+print("Hello coders")
+print("hello nikhil")
+name= "NIkhil"
+age= 18
+age=32.0
+is_adult=True
+
+print(name)
+print(age)
---
+++ python learning/apnacollegetutorial/exercise.py
@@ -0,0 +1,9 @@
+firstname= "Tony"
+lastname="stark"
+print(firstname)
+
+print(lastname)
+age=51
+print(age)
+is_genious=True
+print(is_genious)
