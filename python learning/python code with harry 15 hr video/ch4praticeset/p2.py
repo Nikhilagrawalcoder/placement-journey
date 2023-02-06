3d468ba3760be300a235b39d4fff789785011873
Nikhil <nikhilagrawal0605@gmail.com>
2023-02-06 16:25:17 +0530

    add

---
+++ python learning/python code with harry 15 hr video/ch4praticeset/p2.py
@@ -0,0 +1,9 @@
+m1=int(input("enter student number1"))
+m2=int(input("enter student number2"))
+m3=int(input("enter student number3"))
+m4=int(input("enter student number4"))
+m5=int(input("enter student number5"))
+m6=int(input("enter student number6"))
+list=[m1,m2,m3,m4,m5,m6]
+list.sort()
+print(list)
---
+++ python learning/python code with harry 15 hr video/ch5/dictionary.py
@@ -0,0 +1,5 @@
+mydict={
+    "Fast":"in a quick manner",
+  "Harry":"it is good coder"
+}
+print(mydict['Harry'])
