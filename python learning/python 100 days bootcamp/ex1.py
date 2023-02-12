c1975864814179d52fadc65f21f4dcc62eaf5100
Nikhil <nikhilagrawal0605@gmail.com>
2023-02-12 14:13:20 +0530

    add

---
+++ python learning/python 100 days bootcamp/ex1.py
@@ -0,0 +1,11 @@
+# to make a program to calcualte
+a= int(input("enter first number"))
+b= int(input("enter second number"))
+
+print("the value of a +b is %d",a+b)
+print("the value of a -b is %d",a-b)
+print("the value of a *b is %d",a*b)
+print("the value of aexpb is %d",a**b)
+print("the value of a/b is %d",a/b)
+print("the value of a//b is %d",a//b)
+print("the value of amodulob is %d",a%b)
---
+++ python learning/python 100 days bootcamp/typecasting.py
@@ -0,0 +1,10 @@
+a=int("2")
+b=int("1")
+print(a+b)
+# implict typecasting
+a=5
+b=2
+print(a/b)
+a=5
+b=5
+print(float(a/b))
