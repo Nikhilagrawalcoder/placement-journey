a811bb39f927a5875834ff040896b9b369217591
Nikhil <nikhilagrawal0605@gmail.com>
2023-02-14 22:49:06 +0530

    add

--- python learning/python 100 days bootcamp/ex1.py
+++ python learning/python 100 days bootcamp/ex1.py
@@ -1,8 +1,8 @@
 # to make a program to calcualte
-a= int(input("enter first number"))
-b= int(input("enter second number"))
+a= eval(input("enter first number"))
+b= eval(input("enter second number"))
 
-print("the value of a +b is %d",a+b)
+
 print("the value of a -b is %d",a-b)
 print("the value of a *b is %d",a*b)
 print("the value of aexpb is %d",a**b)
