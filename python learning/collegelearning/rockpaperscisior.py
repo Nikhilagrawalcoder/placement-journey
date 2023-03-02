b27691c008f9d3e84f80629adbd30a211d87cb22
Nikhil <nikhilagrawal0605@gmail.com>
2023-03-02 22:33:03 +0530

    add

--- python learning/collegelearning/rockpaperscisior.py
+++ python learning/collegelearning/rockpaperscisior.py
@@ -1,27 +1,8 @@
-import random
-a=(input("enter player 1 name"))
-# b=(input("enter player 2 name "))
-b="computer"
-c=(input("enter player one choice"))
-
-
-randno= random.randint(1, 3) 
-if randno == 1:
-    d = 'rock'
-elif randno == 2:
-    d= 'paper'
-elif randno == 3:
-    d = 'scisoor'
-
-
-
-
-
-
-if((c== 'rock' and d=='scisoor') or (c=='scisoor' and d=='paper') or(c=='paper' and d=='rock')):
-    print(f'{a}player wins')
-if((d== 'rock' and c=='scisoor') or (d=='scisoor' and c=='paper') or(d=='paper' and c=='rock')):
-    print(f'{b}player wins')    
-else:
-    print(f'{b} player tie with {a} player')
-
+a=input()
+b=input()
+c=input()
+d=input()
+if((c=='rock'and d=='scisoor') or (c=='paper' and d=='stone' ) or (c=='scisoor' and d=='paper')):
+    print(f'{a} Win')
+elif((c=='scisoor' and d=='rock') or (c=='stone' and d=='paper') or (c=='paper' and d=='scisoor')):
+    print(f'{b} Win')
